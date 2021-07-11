4d48c01a833650525164fd6199dbee264dc7157a
Abhishek Bharti <abhishekbharti9910@gmail.com>
2021-07-11 22:48:08 +0530

    added events for the divs in the container

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var container = document.getElementById("mainContain");//grid container
 var slider = document.getElementById("slider");
-
-// def function for deafault pixel
-window.onload = () => {
+const Hcolors = [1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+const randomcolors = ["#f60100", "#002540", "#000000", "#0070f7"];
+const btns = document.querySelectorAll("button");
+var div;
+// def function for deafault on load pixel
+dfltByNature = () => {
     container.style.gridTemplateColumns = "repeat(4,1fr)";
     for (let i = 0; i < 4*4; i++) {
-        let div = document.createElement("div");
+        div = document.createElement("div");
         div.classList.add("pixel");
-        container.insertAdjacentElement("beforeend",div);       
+        container.insertAdjacentElement("beforeend",div);      
     }
 };
 
@@ -18,15 +21,57 @@ slider.addEventListener("mouseup",()=>{
     container.style.gridTemplateColumns = `repeat(${k},1fr)`;
     container.style.gridTemplateRows = `repeat(${k},1fr)`;
     for (let i = 0; i < k*k; i++) {
-        let div = document.createElement("div");
+        div = document.createElement("div");
         div.classList.add("pixel");
         container.insertAdjacentElement("afterbegin",div);
     }
+   active(); 
 });
+
+
 // rmv grid before adding
 function rmvGrid() {
     let divToRmv = document.getElementsByClassName("pixel");
         Array.from(divToRmv).forEach(e => {
             e.remove();
         });
+        
+};
+// adding hover color filling in the pixels
+dfltByNature();
+
+const active = () => {
+    let pixels = document.querySelectorAll(".pixel");
+    console.log(pixels);
+    Array.from(pixels).forEach(pxl => {
+        pxl.addEventListener("mouseover", (e) =>{
+            let color = hexGen();
+            
+            e.target.style.backgroundColor = color;
+        })
+    });
+    
+};  
+
+// hexcolor generator
+function hexGen() {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        SinglHCode = Hcolors[Math.floor(Math.random() * Hcolors.length)];
+        hexColor += SinglHCode;
+    }
+    return hexColor;
 }
+// random color gen
+function randomGen() {
+    let ran = randomcolors[Math.floor(Math.random() * randomcolors.length)];
+    return ran
+}
+// greyScale gen
+// function greyScale() {
+//     let color = "black";
+//     opacity = 0.1;
+    
+// }
+
+active();
